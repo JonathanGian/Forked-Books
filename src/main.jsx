2e65d2fd73f181f3b1eb9911d847e47d9a2ccb24
Jonathan Gian <153126165+JonathanGian@users.noreply.github.com>
2025-01-09 20:47:34 +0200

    Removed old dependencies from main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,15 +3,11 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { CssBaseline } from '@mui/material'
-import { LocalizationProvider } from '@mui/x-date-pickers'
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
 
 // Rendering the App component
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <CssBaseline />
-    <LocalizationProvider dateAdapter={AdapterDayjs}>
     <App />
-    </LocalizationProvider>
   </React.StrictMode>,
 )
